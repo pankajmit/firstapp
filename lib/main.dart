f00d17d678b68e97ff476947c033b6670371d942
Pankaj Mittal <pankajmit@hotmail.com>
2023-06-19 22:46:36 -0700

    checking...

--- lib/main.dart
+++ lib/main.dart
@@ -102,11 +102,11 @@ class _MyHomePageState extends State<MyHomePage> {
           //
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          // wireframe for each widget...
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this ?? many times:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
